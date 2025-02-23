7ad2a69b8dbb3c71772a70b32778650832a2df9b
Jakob Unger <jakob@linux.fritz.box>
2025-02-23 20:33:09 +0100

    Initial commit

---
+++ src/main/java/net/juhumc/therandompack/TheRandomPackDataGenerator.java
@@ -0,0 +1,11 @@
+package net.juhumc.therandompack;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TheRandomPackDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
---
+++ src/main/java/net/juhumc/therandompack/TheRandomPackModClient.java
@@ -0,0 +1,10 @@
+package net.juhumc.therandompack;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TheRandomPackModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
